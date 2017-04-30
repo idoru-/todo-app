ceff71cb3732f7aa8d0cfcbd4fd0d0ae59e821be
idoru- <idoru.maria@gmail.com>
2017-04-30 17:15:48 +0200

    create app.js

---
+++ html/public/javascripts/app.js
@@ -0,0 +1,15 @@
+var TodoList = React.createClass({
+  render: function() {
+      return (
+        <div className="todoListMain">
+          <div className="header">
+            <form>
+              <input placeholder="enter task">
+              </input>
+              <button type="submit">add</button>
+            </form>
+          </div>
+        </div>
+      );
+    }
+});
